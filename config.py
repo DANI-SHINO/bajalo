a4f8b76ee58fc6b57fbdd01e496ee79d0232533e
DANIEL <147718556+DANI-SHINO@users.noreply.github.com>
2025-07-04 18:51:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-import os
-
 class Config:
-    # Clave secreta
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-super-segura'
 
-    # URL de conexión (sin ?ssl-mode=REQUIRED)
-    SQLALCHEMY_DATABASE_URI = os.environ.get('mysql+pymysql://avnadmin:AVNS_xlj5WYOprcR-tFN6xX1@mysql-1b6c1203-mirandadiazjesusdaniel-8cf4.k.aivencloud.com:21093/defaultdb')
+    # Aquí la URL va directa, SIN os.environ.get()
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_xlj5WYOprcR-tFN6xX1@mysql-1b6c1203-mirandadiazjesusdaniel-8cf4.k.aivencloud.com:21093/defaultdb'
 
-    # Opciones de SQLAlchemy (usa SSL de forma correcta)
     SQLALCHEMY_ENGINE_OPTIONS = {
         'connect_args': {
             'ssl': {
@@ -18,7 +14,6 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Configuración de correo (ajusta si la usas)
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
