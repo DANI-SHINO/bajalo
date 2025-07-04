b260771ab5af873041c5b5756744d88bf4c8d13f
DANIEL <147718556+DANI-SHINO@users.noreply.github.com>
2025-07-04 18:58:51 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
-import os 
+import os
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-super-segura'
 
-    # Aquí la URL va directa, SIN os.environ.get()
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_xlj5WYOprcR-tFN6xX1@mysql-1b6c1203-mirandadiazjesusdaniel-8cf4.k.aivencloud.com:21093/defaultdb'
+    SQLALCHEMY_DATABASE_URI = (
+        'mysql+pymysql://avnadmin:AVNS_xlj5WYOprcR-tFN6xX1@'
+        'mysql-1b6c1203-mirandadiazjesusdaniel-8cf4.k.aivencloud.com:21093/defaultdb'
+    )
 
     SQLALCHEMY_ENGINE_OPTIONS = {
         'connect_args': {
             'ssl': {
-                'ca': '/etc/ssl/certs/ca-certificates.crt'
+                'ssl_verify_cert': False,     # 👈 evita verificación de cert
+                'ssl_check_hostname': False   # 👈 evita verificación de hostname
             }
         }
     }
